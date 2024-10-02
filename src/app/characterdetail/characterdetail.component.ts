6ad2d7fbf98591b79a84fee09599c2ba95d6e68d
hoanghung15 <hungaovai15122003@gmail.com>
2024-10-02 18:56:21 +0700

    update header and shoppingcart

--- src/app/characterdetail/characterdetail.component.ts
+++ src/app/characterdetail/characterdetail.component.ts
@@ -3,6 +3,7 @@ import { MainheaderComponent } from '../mainheader/mainheader.component';
 import { MainfooterComponent } from '../mainfooter/mainfooter.component';
 import { CommonModule, NgClass } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-characterdetail',
@@ -19,9 +20,11 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 })
 export class CharacterdetailComponent implements OnInit {
   characterID: any = sessionStorage.getItem('characterID');
+  userID: any = sessionStorage.getItem('userID');
   private apiUrl = 'http://localhost/testAPI/api.php';
   character: any;
-  constructor(private http: HttpClient) {}
+  isViewModel3D = false;
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
   ngOnInit(): void {
     sessionStorage.setItem('test', this.characterID);
     this.getCharacterInfo(this.characterID);
@@ -36,6 +39,35 @@ export class CharacterdetailComponent implements OnInit {
       (error: any) => {
         // Thêm kiểu cho tham số error
         console.error('Có lỗi khi lấy thông tin nhân vật:', error);
+      }
+    );
+  }
+  clickViewModel3D() {
+    this.isViewModel3D = !this.isViewModel3D;
+    console.log(this.character.urlView);
+  }
+  photoURL() {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(
+      this.character.urlView
+    );
+  }
+  addToCart() {
+    const characterID = this.characterID;
+    const userID = this.userID;
+    const dateAdded = new Date().toISOString().slice(0, 19).replace('T', ' '); // Lấy ngày hiện tại
+    const cartData = {
+      userID: userID,
+      characterID: characterID,
+      dateAdded: dateAdded,
+    };
+    this.http.post(`${this.apiUrl}/addToCart`, cartData).subscribe(
+      (response: any) => {
+        console.log('Đã thêm vào giỏ hàng:', response);
+        alert('Thêm vào giỏ hàng thành công!');
+      },
+      (error: any) => {
+        console.error('Có lỗi khi thêm vào giỏ hàng:', error);
+        alert('Lỗi khi thêm vào giỏ hàng.');
       }
     );
   }
--- src/app/mainheader/mainheader.component.ts
+++ src/app/mainheader/mainheader.component.ts
@@ -1,19 +1,84 @@
 import { CommonModule, NgClass } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-mainheader',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, NgClass, CommonModule],
+  imports: [RouterLink, RouterOutlet, NgClass, CommonModule, HttpClientModule],
   templateUrl: './mainheader.component.html',
   styleUrl: './mainheader.component.css',
 })
-export class MainheaderComponent {
+export class MainheaderComponent implements OnInit {
   avtUrl = sessionStorage.getItem('avtUrl');
+  isOpenShoppingCart = false;
   isOpenMenu = false;
+  totalPrice: number | null = null;
+  charactersIncart: any[] = [];
+  userID: any = sessionStorage.getItem('userID');
+  private apiUrl = 'http://localhost/testAPI/apiCharacterIncart.php';
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getShoppingCartCharacters();
+  }
   onToggleMenu() {
     this.isOpenMenu = !this.isOpenMenu;
     console.log(this.avtUrl);
+  }
+  onToggleShoppingCard() {
+    this.isOpenShoppingCart = !this.isOpenShoppingCart;
+    console.log('dm' + this.userID);
+  }
+  mapCharacterData(characterData: any[]): any[] {
+    return characterData.map((character: any) => ({
+      id: character.id,
+      name: character.name,
+      type: character.type,
+      namemovie: character.namemovie,
+      author: character.author,
+      downloadsize: character.downloadsize,
+      texture: character.texture,
+      uvlayer: character.uvlayer,
+      vertices: character.vertices,
+      rigged: character.rigged,
+      price: character.price,
+      urlView: character.urlView,
+      urlBg: character.urlBg,
+      countofsold: character.countofsold,
+      imgUrl: character.imgUrl,
+      bgUrlTop: character.bgUrlTop,
+      uploadAt: character.uploadAt,
+      typeModel: character.typeModel,
+    }));
+  }
+  getShoppingCartCharacters(): void {
+    if (this.userID) {
+      this.http
+        .get(`${this.apiUrl}?userID=${this.userID}`)
+        .subscribe((respone: any) => {
+          this.charactersIncart = this.mapCharacterData(
+            respone.charactersInCart
+          );
+          this.totalPriceCal();
+          console.log(this.charactersIncart);
+        });
+    }
+  }
+  totalPriceCal(): void {
+    this.totalPrice = this.charactersIncart.reduce((sum, character) => {
+      const price = parseFloat(character.price) || 0; // Chuyển đổi price thành float
+      return sum + price; // Cộng giá của từng nhân vật
+    }, 0);
+
+    // Kiểm tra nếu totalPrice là null hoặc undefined, gán nó thành 0
+    if (this.totalPrice !== null && this.totalPrice !== undefined) {
+      // Làm tròn totalPrice đến 2 chữ số thập phân
+      this.totalPrice = parseFloat(this.totalPrice.toFixed(2));
+    } else {
+      // Nếu totalPrice là null, gán nó thành 0
+      this.totalPrice = 0;
+    }
   }
 }
